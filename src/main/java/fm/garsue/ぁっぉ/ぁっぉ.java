1056b681e529778b07e0fdb1826caf34971a4e50
UENISHI Kota <kuenishi@gmail.com>
2016-04-27 23:25:23 +0900

    Try Sparkjava

--- src/main/java/fm/garsue/ぁっぉ/ぁっぉ.java
+++ src/main/java/fm/garsue/ぁっぉ/ぁっぉ.java
@@ -1,15 +1,37 @@
 package fm.garsue.ぁっぉ;
 
+import java.util.Random;
+
+import static spark.Spark.*;
+
 public final class ぁっぉ {
     public static void main(String... args) throws Exception {
-        ぁっぉ ぁっぁっぉ = new ぁっぉ();
-        ぁっぁっぉ.ぉゃっょ();
+        //ぁっぉ ぁっぁっぉ = new ぁっぉ();
+        //ぁっぁっぉ.ぉゃっょ();
+        Random r = new Random();
+        port(8080);
+        get("/", (req, res) -> "Hello " + 名前());
+        get("/ぁっぉ", (req, res) -> "ぁっぉ〜");
+        get("/ぁっぉ〜", (req, res) -> {
+            if (r.nextInt(8) == 7) {
+                return "ぁっぉぁっぉうるさいぞｗｗｗｗ";
+            } else {
+                return "ぁっぉ〜";
+            }
+        });
+        staticFileLocation("");
+        exception(Exception.class, (e, req, res) ->
+        {
+            res.status(404);
+            res.body("Resource not found");
+        });
+
     }
 
     public static String 名前() {
         return "ぁっぉ";
     }
-
+/*
     private ぁっぉHTTPサーバー ぁつぁっ = null;
     public ぁっぉ(){
         this.ぁつぁっ = new ぁっぉHTTPサーバー(false, 8080);
@@ -17,4 +39,5 @@ public final class ぁっぉ {
     public void ぉゃっょ() throws Exception {
         this.ぁつぁっ.走る();
     }
+    */
 }
